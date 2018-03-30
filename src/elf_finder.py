383e0f8a9c43b5e390925f92feb497ea458dac46
Sudneo <mucci.daniele@gmail.com>
2018-03-30 22:39:53 +0300

    fixes

--- src/elf_finder.py
+++ src/elf_finder.py
@@ -1,5 +1,6 @@
 import os
 import magic
+from stopit import SignalTimeout as Timeout
 
 class elf_finder:
 
@@ -28,9 +29,15 @@ class elf_finder:
         :return: True if the file is ELF, false otherwise
         """
         try:
-            filetype=magic.from_file(path)
-            if "ELF" in filetype:
-                return True
+             # filetype=magic.from_file(path)
+             # if "ELF" in filetype:
+             #     return True
+            with Timeout(5.0) as timeout_ctx:
+                bytes = open(path,"rb").read(4).encode('hex')
+                if bytes == "7f454c46":
+                    return True
+                else:
+                    return False
         except:
             print "[WARNING] Couldn't determine if "+path+" is ELF: is_elf failed"
 
