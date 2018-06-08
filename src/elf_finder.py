2b826a01fc3c75a07c8ca7b9b27625a72bab8357
Sudneo <mucci.daniele@gmail.com>
2018-06-08 20:25:32 +0300

    Reformats ELF-finder

--- src/elf_finder.py
+++ src/elf_finder.py
@@ -1,51 +1,51 @@
+import logging
 import os
 from stopit import SignalTimeout as Timeout
 from stopit import TimeoutException
 
-class elf_finder:
+"""
+Utilities to find all ELFs files in one given directory (default root)
+A file is considered ELF if it has the 7f454c46 magic number at the beginning.
+"""
 
-    root_dir=""
+def is_elf(path):
+    """
 
-    def __init__(self,dir):
-        self.root_dir=dir
+    :param path: the path to the file to analyze
+    :return: True if the file is ELF, false otherwise
+    """
+    try:
+        with Timeout(1.0) as timeout_ctx:
+            try:
+                bytes = open(path,"rb").read(4).encode('hex')
+                if bytes == "7f454c46":
+                    return True
+                else:
+                    return False
+            except TimeoutException:
+                logging.warning("Could not open the file"+path+" in 1s. Skipping.")
+    except:
+        logging.error("Couldn't determine if "+path+" is ELF.")
 
-    def get_elf_list(self):
-        """
+    return False
 
-        :return: A list with full paths of every ELF file in the system
-        """
-        elf_files=[]
-        for root,dir, files in os.walk(self.root_dir):
-            for file in files:
-                try:
-                    if os.path.islink(root+"/"+file):
-                        continue
-                    else:
-                        if self.is_elf(root+"/"+file):
-                            elf_files.append(root+"/"+file)
-                except:
+def get_elf_list(root_dir="/"):
+    """
+    :return: A list with full paths of every ELF file in the system
+    """
+    elf_files=[]
+    for root,dir, files in os.walk(root_dir):
+        for file in files:
+            try:
+                if os.path.islink(root+"/"+file):
                     continue
-        return elf_files
+                else:
+                    if is_elf(root+"/"+file):
+                        elf_files.append(root+"/"+file)
+            except:
+                continue
+    return elf_files
 
 
-    def is_elf(self, path):
-        """
 
-        :param path: the path to the file to analyze
-        :return: True if the file is ELF, false otherwise
-        """
-        try:
-            with Timeout(1.0) as timeout_ctx:
-                try:
-                    bytes = open(path,"rb").read(4).encode('hex')
-                    if bytes == "7f454c46":
-                        return True
-                    else:
-                        return False
-                except TimeoutException:
-                    print "[WARNING] 7fCould not open the file"+path+" in 1s. Skipping."
-        except:
-            print "[WARNING] Couldn't determine if "+path+" is ELF."
-
-        return False
 
